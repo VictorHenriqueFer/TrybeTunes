48398b416d209aaa288b0a345d4f5a7ca31c8e12
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-05-29 22:51:51 -0300

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends Component {
+  state = {
+    loading: false,
+    user: {},
+  };
+
+  async componentDidMount() {
+    await this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    this.setState({
+      loading: true,
+    });
+    const perfil = await getUser();
+    this.setState({
+      user: perfil,
+      loading: false,
+    });
+  };
+
   render() {
+    const { loading, user } = this.state;
     return (
-      <div data-testid="page-profile">
+      <section data-testid="page-profile">
         <Header />
-      </div>
+        { loading ? <Loading /> : (
+          <div>
+            <img data-testid="profile-image" src={ user.image } alt="Foto User" />
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.description}</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
+      </section>
     );
   }
 }
